0688ee5f10e0d2014337ebbfe5c1847c56d602e5
Bethany Holley <chocolatechipfaced@gmail.com>
2019-09-27 22:00:00 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,47 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  array = ["I","am","really","learning"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["I","am","really","learning"]
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I","am","really","learning"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I","am","really","learning","arrays!"]
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow","I","am","really","learning"]
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow","I","am","am","learning"]
+  am = array[3]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow","I","am","really","learning"]
+  wow = array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow","I","am","really","learning","arrays!"]
+  arrays = array[5]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array = ["I","am","really","learning"]
+  array[0] = "totally"
 end
